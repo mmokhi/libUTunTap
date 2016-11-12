43baa63dc4630d61128a00247f49df07edb51120
Mahdi Mokhtari <mokhi64@gmail.com>
2016-11-12 10:42:41 +0330

    Init commit

--- tuntap.c
+++ tuntap.c
@@ -10,6 +10,13 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
+ * Credits:
+ * Copyright (c) Tristan Le Guern <tleguern@bouledef.eu>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
